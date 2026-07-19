1b54325358054512346f4e2802c513a4fc9ab455
cqcqcqb <2683422138@qq.com>
2026-07-19 16:39:42 +0800

    chore: bootstrap Spring Boot backend

---
+++ backend/src/test/java/com/agromall/AgriculturalMallApplicationTest.java
@@ -0,0 +1,12 @@
+package com.agromall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalMallApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
